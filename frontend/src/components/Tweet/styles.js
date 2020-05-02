d2bac7a4dddd765d1ed63071cc58c5151717fe3a
henriquesml <henrique_schmeller@hotmail.com>
2020-05-01 21:52:44 -0300

    refatoração post

--- frontend/src/components/Tweet/styles.js
+++ frontend/src/components/Tweet/styles.js
@@ -1,10 +1,66 @@
 import styled from 'styled-components';
 
 export const Container = styled.li`
-  padding: 20px 20px 0;
+  height: 100px;
+  max-height: 200px;
   margin-bottom: 20px;
   background: #FFF;
   border-top: 1px solid #eee;
-  height: 100px;
-  max-height: 200px;
+  border-radius: 4px;
+  
+`;
+
+export const Header = styled.header`
+  height: 35px;
+  display: flex;
+  align-items: center;
+  padding: 15px;
+`;
+
+export const Author = styled.strong`
+  color: #333;
+`;
+
+export const CurrentDate = styled.h2`
+  color: #333;
+  font-size: 14px;
+  font-weight: 100;
+
+`;
+
+export const Content= styled.div`
+  display: flex;
+  height: calc(100% - 70px);
+  padding-left: 15px;
+  align-items: center;
+`;
+
+export const ContentPost = styled.p`
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+export const Footer = styled.footer`
+  height: 35px;
+  display: flex;
+  align-items: center;
+  padding-left: 15px;
+`;
+
+export const Like = styled.button`
+  border: 0;
+  background: transparent;
+
+  display: flex;
+  align-items: center;
+  color: #697882;
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.7;
+  }
+
+  img {
+    margin-right: 5px;
+  }
 `;
