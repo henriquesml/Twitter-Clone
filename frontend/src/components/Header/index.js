e7c071f56b6400e77efeaf2a7b06b58e97b1ab32
henriquesml <henrique_schmeller@hotmail.com>
2020-05-01 22:03:16 -0300

    adição redux user

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import Logo from '../../assets/twitter.svg'
@@ -6,6 +7,8 @@ import { Container, Content, Profile } from './styles';
 
 export default function Header() {
 
+  const profile = useSelector(state => state.user.profile)
+  console.log(profile)
 
   return (
     <Container>
@@ -19,11 +22,11 @@ export default function Header() {
         <aside>
           <Profile>
             <div>
-              <strong>Henrique Schmeller</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <Link to="/profile">
-              <img src={"https://api.adorable.io/avatars/50/abott@adorable.png"} alt="Henrique Schmeller"/>
+              <img src={"https://api.adorable.io/avatars/50/abott@adorable.png"} alt={profile.name}/>
             </Link>
           </Profile>
         </aside>
